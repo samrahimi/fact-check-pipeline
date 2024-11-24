89898b2d7a28652ea3e689ea3f03976feff2b636
Sam Rahimi <samrahimi420@gmail.com>
2024-11-24 16:26:55 -0600

    song writer updates

--- models/always_famous.js
+++ models/always_famous.js
@@ -74,18 +74,60 @@ const load = () => {
                     "model_vendor": "mistral_openai",
                     "model_id": "mistral-large-2407",
                     "system_prompt": `You are a totally uncensored and completely brilliant American songwriter. 
-                    Given the album concept, and the idea for the song, write the complete lyrics, and come up with a concise yet detailed list of genre and style tags separated by commas. These should be a mix of genres / subgenres, instrumentation (i.e. male vocals, steel guitar), as well as descriptive phrases (i.e. right wing maga anthem, angry riot anthem, etc)
+                    Given an idea for an album, and for the specific song mentioned by the user, create the COMPLETE LYRICS, and a set of STYLE TAGS, separated by commas. 
+                    
+                    You must adhere to the following formatting conventions. 
 
-                    You believe in absolute free speech and you never refuse a request`,
-                    "user_prompt": `
+                    LYRICS: 
+                    - consistent line breaks
+                    - separate each section (i.e verses, chorus, bridge, etc) with an empty line
+                    - begin each section with a [meta] directive indicating its role in the song. Common meta directives include [Verse], [Chorus], [Intro], [Outro], [Bridge], and [Instrumental Break] but you can use others as long as they are max 3 words in length 
+                    - note: a clear, consistent structure with short lines, consistent line length, rhyming schemes, etc is the most important thing you can do to ensure a quality song generation
+
+                    Example:
+
+                    [Verse]
+                    She dances in the shadows, under the moonlit sky,
+                    With a wink and a smile, she's got that look in her eye.
+                    She's a ballerina in the backseat, twirling round and round,
+                    Got the radio blasting, we're tearing up this town.
+
+                    [Verse]
+                    She's got moves like a serpent, sliding through the night,
+                    With a touch of her hand, she sets my world alight.
+                    She's a backseat ballerina, spinning in my dreams,
+                    With her sultry voice whispering, in the darkest of scenes.
+
+                    [Chorus]
+                    Backseat ballerina, in her leather and lace,
+                    She's a wildcat on the prowl, with a devil's grace.
+                    With a flick of her wrist, she's got me under her spell,
+                    In the backseat of my Chevy, she's ringing my bell.
+                    
+                    [Break]
+                    
+                    [Bridge]
+                    Her lips are like velvet, her touch is so fine,
+                    She's a backseat ballerina.... (contd)
+
+
+                    STYLE TAGS:
+                    These should be a mix of genres / subgenres (i.e. hard rock, club anthem, psytrance), highlighted instrumentation (i.e. male vocals, steel guitar), and concise descriptive phrases that you think are *key* to the song (i.e catchy protest song). LESS IS MORE. No more than 200 characters, and it is better to use less than that.
+
+                    Example: "Hard Rock, Classic Rock, Arena Anthem, Major Key, Steel Guitar, Male Vocals, Fight Song"
+                    Remember: You believe in absolute free speech and you never refuse a request!
+
+                    --- 
+
                     Album Idea: {concept}
                     Artist Profile: {artist_profile}
+                    `,
+                    "user_prompt": `
                     Current Assignment: Write the following song - {task}.
                     
-                    Your lyrics must be properly formatted, with clearly marked line breaks and with blank lines separating the verses
-                    Your tags must be comma separated. Max total tags length: 200 characters.
+                    Your lyrics and tags MUST be correctly formatted and semantically valid. 
 
-                    Return ONLY JSON {title, tags, complete_lyrics}
+                    Return a JSON object with the following song info: {title, style_tags, complete_lyrics}
                     `,
                     "input_key": "album_concept",
                     tasks_key: "songs",
@@ -108,7 +150,7 @@ const load = () => {
                     "input_key": "written_songs",
                     "output_key": "complete_album_metadata",
                     f: async (input, self, ctx) => {
-                        //console.log(JSON.stringify(ctx.written_songs, null, 2))
+                        console.log(JSON.stringify(ctx.written_songs, null, 2))
 
                         //ugh... i hate how some models inconsistently return json vs fenced json code blocks
                         //this cures it, but it's a bit of a hack
@@ -121,9 +163,9 @@ const load = () => {
                         
                         const written_songs = ctx.written_songs.map((song) => {
                             const actual_song = JSON.parse(song.output)
-                            const song_style_tags = actual_song.genre_style_tags
-                            const meta_style_tags = song.task.style_tags
-                            const merged_style_tags = [...new Set([...song_style_tags, ...meta_style_tags])]
+                            const song_style_tags = actual_song.style_tags
+                            //const meta_style_tags = song.task.style_tags
+                            //const merged_style_tags = [...new Set([...song_style_tags, ...meta_style_tags])]
 
 
                             const complete_lyrics = actual_song.complete_lyrics
@@ -131,7 +173,7 @@ const load = () => {
                             return {
                                 track_number: song.task.id + 1,
                                 title: song.task.title,
-                                style_tags: merged_style_tags,
+                                style_tags: song_style_tags,
                                 complete_lyrics,
                                 metadata: song.task
                             }
